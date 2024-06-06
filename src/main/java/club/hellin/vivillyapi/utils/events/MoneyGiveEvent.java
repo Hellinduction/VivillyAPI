5d0525da603049b431de8b48991683383bdbfa3a
Hellinduction <joseph.connor.west@icloud.com>
2024-06-06 07:20:24 +0100

    oops

--- src/main/java/club/hellin/vivillyapi/utils/events/MoneyGiveEvent.java
+++ src/main/java/club/hellin/vivillyapi/utils/events/MoneyGiveEvent.java
@@ -2,23 +2,23 @@ package club.hellin.vivillyapi.utils.events;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.bukkit.entity.Player;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
-import org.bukkit.event.player.PlayerEvent;
 
 @Getter
 @Setter
-public final class MoneyGiveEvent extends PlayerEvent implements Cancellable {
+public final class MoneyGiveEvent extends Event implements Cancellable {
     private static final HandlerList handlers = new HandlerList();
 
-    private boolean cancelled = false;
+    private final OfflinePlayer who;
 
+    private boolean cancelled = false;
     private double amount;
 
-    public MoneyGiveEvent(final Player who, final double amount) {
-        super(who);
-
+    public MoneyGiveEvent(final OfflinePlayer who, final double amount) {
+        this.who = who;
         this.amount = amount;
     }
 
